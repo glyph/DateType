e02201b795ed90b03cabe031ece738a1c011daef
Maciej Olko <maciej.olko@affirm.com>
2023-08-22 01:55:52 +0200

    Workaround ZoneInfo/mypy issue

--- tryit.py
+++ tryit.py
@@ -1,6 +1,6 @@
 from typing import cast
 from datetime import datetime, timezone, date, time
-from zoneinfo import ZoneInfo
+from backports.zoneinfo._zoneinfo import ZoneInfo
 from datetype import (
     Date,
     DateTime,
